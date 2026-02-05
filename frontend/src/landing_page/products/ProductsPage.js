954c05ebc5703ebf7aae2becad71e4b089244c53
MOHAMMAD-SAIF1 <saifmohammad2237@gmail.com>
2026-02-05 10:12:42 +0530

    Setup Configuration

---
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Hero from './Hero';
+import LeftSection from './LeftSection';
+import RightSection from './RightSection';
+import Footer from '../Footer';
+import Universe from './Universe';
+
+
+function PricingPage() {
+    return ( 
+        <>
+        <Navbar />
+        <Hero />
+        <LeftSection />
+        <RightSection />
+        <Universe />
+        <Footer />
+        </>
+     );
+}
+
+export default PricingPage;
