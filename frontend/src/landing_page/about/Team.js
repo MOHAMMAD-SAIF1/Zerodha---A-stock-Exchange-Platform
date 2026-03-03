6fa504e5bcb3db2b3ab050c96646d31252984575
MOHAMMAD-SAIF1 <saifmohammad2237@gmail.com>
2026-03-03 15:00:55 +0530

    About Page Modification

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -3,34 +3,26 @@ import React from 'react';
 function Team() {
     return ( 
          <div className="container">
-      <div className="row p-5 mb-5 mt-5 border-top">
+      <div className="row p-3  mt-5 border-top">
         <h1 className="text-center">
           People
         </h1>
       </div>
 
-      <div className="row p-5 mt-5  text-muted " style={{lineHeight: "1.8", fontSize: "1.2em"}}>
-        <div className="col-6 p-5">
-            <img src="media/images/nithinKamath.jpg" style={{borderRadius:"100%"}}/>
+      <div className="row p-3   text-muted " style={{lineHeight: "1.8", fontSize: "1.2em"}}>
+        <div className="col-6 p-5 text-center">
+            <img src="media/images/nithinKamath.jpg" style={{borderRadius:"100%",width:"50%"}}/>
+            <h4 className="mt-5">Nithin Kamath</h4>
+            <h6>Founder & CEO</h6>
         </div>
         <div className="col-6 p-5">
-          <p>
-            In addition, we run a number of popular open online educational and
-            community initiatives to empower retail traders and investors.
-          </p>
+        <p> Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.</p>
 
-          <p>
-            <a href="" style={{textDecoration:"none"}}>Rainmatter</a>, our fintech fund and incubator, has invested in several
-            fintech startups with the goal of growing the Indian capital
-            markets.
-          </p>
+        <p> He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
 
-          <p>
-            
-            And yet, we are always up to something new every day. Catch up on
-            the latest updates on our blog or see what the media is saying about
-            us or learn more about our business and product philosophies.
-          </p>
+         <p>Playing basketball is his zen.</p>
+
+         <p>Connect on <a href="">Homepage</a> / <a href="">TradingQnA</a> / <a href="">Twitter</a></p>
         </div>
       </div>
     </div>
