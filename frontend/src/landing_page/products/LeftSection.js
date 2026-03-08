0bd125b9ec0f5efcb7fa0a5d18ad692ba122b513
MOHAMMAD-SAIF1 <saifmohammad2237@gmail.com>
2026-03-08 18:05:23 +0530

    Product Section

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 function LeftSection({imageURL, productName, productDescription, tryDemo, learnMore, googlePlay, appStore}) {
     return (  
-        <div className="container">
+        <div className="container mt-5">
             <div className="row">
-                <div className="col-6 p-3">
+                <div className="col-4 p-5">
                     <img src={imageURL}/>
                 </div>
-                <div className="col-6">
+                <div className="col-6 p-5 mt-5">
                     <h1>{productName}</h1>
                     <p>{productDescription}</p>
-                    <a href={tryDemo}></a>
+                    <div>
+                     <a href={tryDemo}>Try Demo</a>
+                     <a href={learnMore} style={{marginLeft:"50px"}}>Learn More</a>
+                    </div>
+                    <div mt-3>
+                      <a href={googlePlay}><img src="media/images/googlePlayBadge.svg"/></a>
+                       <a href={appStore}><img src="media/images/appstoreBadge.svg" style={{marginLeft:"50px"}}/></a>
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import Navbar from '../Navbar';
-import Hero from './Hero';
-import LeftSection from './LeftSection';
-import RightSection from './RightSection';
-import Footer from '../Footer';
-import Universe from './Universe';
-
+import React from "react";
+import Navbar from "../Navbar";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Footer from "../Footer";
+import Universe from "./Universe";
 
 function PricingPage() {
-    return ( 
-        <>
-       
-        <Hero />
-        <LeftSection />
-        <RightSection />
-        <Universe />
-    
-        </>
-     );
+  return (
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, and an elegant UI, and more. Enjoy the kite experience on your desktop, mobile, and web."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection  imageURL="media/images/console.png"
+        productName="Console"
+        productDescription="The central dashboard for you Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations. Access your account from any web browser, anywhere in the world."
+        learnMore=""/>
+      <LeftSection
+        imageURL="media/images/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection imageURL="media/images/kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        learnMore=""/>
+      <LeftSection
+        imageURL="media/images/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <Universe />
+    </>
+  );
 }
 
 export default PricingPage;
